430cc5be6401aaeec227107660c4aad55f56d092
PacktDwayne <dwayned@packtpub.com>
2019-08-07 15:46:33 +0530

    Code files added

---
+++ Section 3/3.4/after/DictionaryLookups/LookupsAndExamples/LookupResponse.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+public partial class LookupResponse {
+    [JsonProperty("displaySource")] public string DisplaySource
+
+{
+    get;
+    set;
+}
+
+[JsonProperty("normalizedSource")]
+public string NormalizedSource {
+    get;
+    set;
+}
+
+[JsonProperty("translations")]
+public Translation[] Translations {
+    get;
+    set;
+}
+
+}
+
+public partial class Translation {
+    [JsonProperty("backTranslations")] public BackTranslation[] BackTranslations
+
+{
+    get;
+    set;
+}
+
+[JsonProperty("confidence")]
+public long Confidence {
+    get;
+    set;
+}
+
+[JsonProperty("displayTarget")]
+public string DisplayTarget {
+    get;
+    set;
+}
+
+[JsonProperty("normalizedTarget")]
+public string NormalizedTarget {
+    get;
+    set;
+}
+
+[JsonProperty("posTag")]
+public string PosTag {
+    get;
+    set;
+}
+
+[JsonProperty("prefixWord")]
+public string PrefixWord {
+    get;
+    set;
+}
+
+}
+
+public partial class BackTranslation {
+    [JsonProperty("displayText")] public string DisplayText
+
+{
+    get;
+    set;
+}
+
+[JsonProperty("frequencyCount")]
+public long FrequencyCount {
+    get;
+    set;
+}
+
+[JsonProperty("normalizedText")]
+public string NormalizedText {
+    get;
+    set;
+}
+
+[JsonProperty("numExamples")]
+public long NumExamples {
+    get;
+    set;
+}
+
+}
+
+public partial class LookupResponse {
+    public static LookupResponse[] FromJson(string json) => JsonConvert.DeserializeObject<LookupResponse[]>(json, Converter.Settings);
+}
+
+public static class Serialize {
+    public static string ToJson(this LookupResponse[] self) => JsonConvert.SerializeObject(self, Converter.Settings);
+}
+
+internal static class Converter {
+    public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+
+{
+    MetadataPropertyHandling = MetadataPropertyHandling.Ignore, DateParseHandling = DateParseHandling.None, Converters =
+
+{
+    new IsoDateTimeConverter
+
+{
+    DateTimeStyles = DateTimeStyles.AssumeUniversal
+}
+
+}
+,
+}
+;
+}
---
+++ Section 3/3.4/after/DictionaryLookups/LookupsAndExamples/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace LookupsAndExamples
+{
+    class MainClass
+    {
+        const string _subscriptionKey = "b4d009edae8f42398f2ea893a9f4017d";
+        const string _host =
+            "https://api.cognitive.microsofttranslator.com";
+
+        public static void Main(string[] args)
+        {
+            LookupAlternatives("gardens", "en", "fr");
+        }
+
+        static void LookupAlternatives(string word, string source, string target)
+        {
+            string route = 
+                $"/dictionary/lookup?api-version=3.0&from={source}&to={target}";
+
+            System.Object[] body = new System.Object[] { new { Text = word } };
+            var requestBody = JsonConvert.SerializeObject(body);
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method = HttpMethod.Post;
+
+                request.RequestUri = new Uri(_host + route);
+
+                request.Content = new StringContent(requestBody, 
+                    Encoding.UTF8, "application/json");
+
+                request.Headers.Add("Ocp-Apim-Subscription-Key", 
+                    _subscriptionKey);
+
+                var response = client.SendAsync(request).Result;
+                var jsonResponse = response.Content.ReadAsStringAsync().Result;
+
+                var lookupResponse = 
+                    JsonConvert.DeserializeObject<List<LookupResponse>>(jsonResponse);
+
+                PrintResults(lookupResponse);
+                Console.ReadKey();
+            }
+        }
+
+        private static void PrintResults(List<LookupResponse> lookupResponse)
+        {
+    
+            foreach (var response in lookupResponse)
+            {
+                Console.WriteLine($"Original word: {response.DisplaySource}\n");
+            
+
+                foreach (var translation in response.Translations)
+                {
+                    Console.WriteLine("Translation\t\tPosTag\n");
+                    Console.WriteLine($"{translation.NormalizedTarget,20}{translation.PosTag,10}");
+                    Console.WriteLine();
+                    Console.WriteLine($"Back translation\t\tNumber of Examples\n");
+                    foreach(var alt in translation.BackTranslations)
+                    {
+                        Console.WriteLine($"{alt.NormalizedText,20}{alt.NumExamples,10}\n");
+                    }
+                }
+            }
+        }
+    }
+}
